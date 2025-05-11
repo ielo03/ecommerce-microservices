3f2a7c63ce2137479f6d86b3f67ede6f8079f905
ielo03 <89800883+ielo03@users.noreply.github.com>
2025-05-11 16:46:30 -0700

    e

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const HOST = process.env.HOST || "0.0.0.0"; // Bind to all network interfaces
 
 // Middleware
 app.use(bodyParser.json());
-app.use((req, res, next) => {
+app.use((req, _, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
