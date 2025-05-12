f9543f526be05a3e26083f0c65b61c6af07f96ea
ielo03 <89800883+ielo03@users.noreply.github.com>
2025-05-11 18:40:04 -0700

    k

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 const api_gateway_version = "${API_GATEWAY_VERSION}";
 
 // API Gateway health endpoint
-app.get("/api-gateway/health", (req, res) => {
+app.get("/api-gateway/health", (_, res) => {
   console.log("API Gateway health endpoint called");
   res.json({
     status: `good ${api_gateway_version}`,
